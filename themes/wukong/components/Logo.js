d985b4ba347b79f851e7238b34bdb7fa908af1f2
mjw-141 <mjw2364808205@gmail.com>
2024-10-11 20:26:19 +0800

    Update Logo.js

--- themes/wukong/components/Logo.js
+++ themes/wukong/components/Logo.js
@@ -1,22 +1,26 @@
-import { siteConfig } from '@/lib/config'
 import Link from 'next/link'
+import Image from 'next/image' // 确保导入 Image 组件
+
 /**
  * Logo
- * 实际值支持文字
+ * 显示图片Logo
  * @param {*} props
  * @returns
  */
-const Logo = props => {
-  const { siteInfo } = props
+const Logo = (props) => {
   return (
     <Link href='/' passHref legacyBehavior>
       <div className='flex flex-col justify-center items-center cursor-pointer space-y-3'>
-        <div className='font-medium text-lg p-1.5 rounded dark:border-white dark:text-white menu-link transform duration-200'>
-          {' '}
-          {siteInfo?.title || siteConfig('TITLE')}
-        </div>
+        <Image
+          src='/WukongIcon.svg' // 确保图片存放在 public 文件夹下
+          alt='Site Logo'
+          width={160} // 设置图片宽度
+          height={100}// 设置图片高度
+          priority={true} // 优先加载该图片
+        />
       </div>
     </Link>
   )
 }
+
 export default Logo
