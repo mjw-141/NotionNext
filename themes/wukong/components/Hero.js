983bcfd8181af09e8015e509172a13ddee4e4f19
mjw-141 <mjw2364808205@gmail.com>
2024-10-11 20:40:02 +0800

    Update Hero.js

--- themes/wukong/components/Hero.js
+++ themes/wukong/components/Hero.js
@@ -90,7 +90,7 @@ const Hero = props => {
       <LazyImage
         id='header-cover'
         alt={siteInfo?.title}
-        src={siteInfo?.pageCover}
+        src="/monkey.jpg"
         className={`header-cover w-full h-screen object-cover object-center ${siteConfig('HEXO_HOME_NAV_BACKGROUND_IMG_FIXED', null, CONFIG) ? 'fixed' : ''}`}
       />
     </header>
