9db810a4ceb0c7ac876eb4fbd4cb3a174ef43a1f
mjw-141 <mjw2364808205@gmail.com>
2024-10-14 20:37:51 +0800

    Update index.js

--- themes/wukong/index.js
+++ themes/wukong/index.js
@@ -168,7 +168,7 @@ const LayoutBase = props => {
               ' w-full mx-auto lg:flex lg:space-x-4 justify-center relative z-10'
             }>
             <div
-              className={`${className || ''} w-full ${fullWidth ? '' : (shrink ? '' : 'max-w-4xl')} h-full overflow-hidden`}>
+              className={`${className || ''} w-full ${fullWidth ? '' : (shrink ? 'max-w-4xl' : 'max-w-6xl')} h-full overflow-hidden`}>
               <Transition
                 show={!onLoading}
                 appear={true}
