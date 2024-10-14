30533a8846806773e2a8a1781f00ea318f8a5cfd
mjw-141 <mjw2364808205@gmail.com>
2024-10-14 19:39:14 +0800

    Create PictureList.js

---
+++ themes/wukong/components/PictureList.js
@@ -0,0 +1,17 @@
+import OverlappingImages from './PictureShadow'
+
+const Try= () =>{
+    const images = [
+        "/1.jpg",
+        "/2.jpg",
+        "/3.jpg",
+        "/4.jpg",
+        "/5.jpg",
+      ];
+   return ( 
+    <div>
+        <OverlappingImages images={images} />
+    </div>)
+}
+
+export default Try
