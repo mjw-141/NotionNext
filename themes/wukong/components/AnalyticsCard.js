e863aaf59d6f012aa39d0b25ad5085d93e4edd61
mjw-141 <mjw2364808205@gmail.com>
2024-10-14 20:21:57 +0800

    Update AnalyticsCard.js

--- themes/wukong/components/AnalyticsCard.js
+++ themes/wukong/components/AnalyticsCard.js
@@ -6,7 +6,7 @@ export function AnalyticsCard (props) {
     <div className='ml-2 mb-3 '>
       <i className='fas fa-chart-area' /> 统计
     </div>
-    <div className='text-xs  font-light justify-center mx-7'>
+    <div className='text-xs  font-light justify-center mx-7 dark:text-white'>
       <div className='inline'>
         <div className='flex justify-between'>
           <div>文章数:</div>
