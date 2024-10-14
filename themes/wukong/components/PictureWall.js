0362c13167c554f7250eb7e4567fc82980d59601
mjw-141 <mjw2364808205@gmail.com>
2024-10-14 20:55:25 +0800

    Update PictureWall.js

--- themes/wukong/components/PictureWall.js
+++ themes/wukong/components/PictureWall.js
@@ -4,7 +4,7 @@ const ImageGrid = () => {
   const containerRef = useRef(null);
   const [images, setImages] = useState([]);
   const imgWidth = 250;
-  const imageCount = 5;
+  const imageCount = 8;
   const handleClick = () => {
     window.location.href = '/article/guide'; // 跳转到指定的页面
   };
